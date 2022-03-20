3cbcf08b14b49af05a171dd26b87bcf1e752f050
Storn25 <nais30055003@gmail.com>
2022-03-20 13:12:39 +0300

    commit

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('signin/', register, name='signin'),
     path('logout', logout_user, name='logout'),
-    path('/<int:pk>/', FolderView.as_view(), name='folder')
+    path('<int:pk>/', FolderView.as_view(), name='folder')
 ]
